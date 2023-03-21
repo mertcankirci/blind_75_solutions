7f54f26ac2fb05da68db1adf42cf0d6385a61431
mertcankirci <mertkirci02@gmail.com>
2023-03-22 00:16:37 +0300

    TwoSum Algorithm solved.

---
+++ twoSum_array.py
@@ -0,0 +1,9 @@
+
+def twoSum(self,nums,target):
+    nums = [2,7,11,15]
+    target = 9
+    for i in range(len(nums)):
+                for j in range(i + 1, len(nums)):
+                    if nums[i] + nums[j] == target:
+                        return [i,j]
+  
